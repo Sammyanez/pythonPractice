7f01f9b6ea57c4a99094e02630912d19bd1dea79
Samuel Yanez <Sammy78504@gmail.com>
2021-07-22 17:14:03 -0500

    bubbleSort Solved

---
+++ bubbleSort.py
@@ -0,0 +1,21 @@
+def bubbleSort(array):
+    swap = True
+    length = len(array) - 1
+    while swap:
+        swap = False
+        for i in range(length):
+            if array[i] > array[i + 1]:
+                var = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = var
+                i += 1
+                swap = True
+            else:
+                i += 1
+    return array
+
+
+
+array2 = [8, 5, 2, 9, 5, 6, 3]
+bubbleSort(array2)
+print("done")
